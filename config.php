51c05f40f9729d997c5ca41b58d9cf4f663c1db1
alwihambali <98443087+alwihambali@users.noreply.github.com>
2022-01-26 15:45:48 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,20 +1,20 @@
 <?php
-/* $username = 'root';
-$password = 'root';
-$dbName = 'test';
-$connectionName = getenv("bss-sandbox-env-1:asia-southeast2:bss-dev-mysql-server");
-$socketDir = getenv('/var/lib/mysql/mysql.sock') ?: '/cloudsql';
+// $username = 'root';
+// $password = 'root';
+// $dbName = 'test';
+// $connectionName = getenv("bss-sandbox-env-1:asia-southeast2:bss-dev-mysql-server");
+// $socketDir = getenv('/var/lib/mysql/mysql.sock') ?: '/cloudsql';
 
-// Connect using UNIX sockets
-$dsn = sprintf(
-    'mysql:dbname=%s;unix_socket=%s/%s',
-    $dbName,
-    $socketDir,
-    $connectionName
-);
+// // Connect using UNIX sockets
+// $dsn = sprintf(
+//     'mysql:dbname=%s;unix_socket=%s/%s',
+//     $dbName,
+//     $socketDir,
+//     $connectionName
+// );
 
-// Connect to the database.
-$conn = new PDO($dsn, $username, $password, $conn_config);*/
+// // Connect to the database.
+// $conn = new PDO($dsn, $username, $password, $conn_config);
 
 $databaseHost = '34.101.142.207';
 $databaseName = 'test';
